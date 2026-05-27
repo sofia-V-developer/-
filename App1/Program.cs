5779f5f6ff8186d3d8406efa4d50f744d66fe47d
Karrwet <artemkascherbakov133@gmail.com>
2026-05-28 02:42:30 +0300

    Try1

---
+++ App1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions; // Бесполезны
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] kar = {1, 54, 12, 6};
+
+            foreach (int item in kar)
+            {
+                System.Console.WriteLine(item);
+            }
+        }  
+    }    
+}
